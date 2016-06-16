46d66fea9ef6d0e5b3a3e9b8323d3605c56c16d2
Anton DeVore <adevore@marchex.com>
2016-06-16 15:06:22 -0700

    adding git aliases and functions

---
+++ git/aliases.bash
@@ -0,0 +1,43 @@
+alias g='git'
+alias ga='git add'
+alias gall='git add .'
+alias gb='git branch'
+alias gba='git branch -a'
+alias gblt='for k in `git branch -r | perl -pe "s/^..(.*?)( ->.*)?$/\1/"`; do echo -e `git show --pretty=format:"%Cgreen%ci %Cred%cr%Creset" $k -- | head -n 1`\\t$k; done | sort -r'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gci='git commit --interactive'
+alias gcl='git clone'
+alias gco='git checkout'
+alias gcount='git shortlog -sn'
+alias gcp='git cherry-pick'
+alias gdel='git branch -D'
+alias gdv='git diff -w "$@" | vim -R -'
+alias get='git'
+alias gexport='git archive --format zip --output'
+alias gg='git grep -n'
+alias ggl='git grep -l'
+alias gl='git pull'
+alias gll='git log --graph --pretty=oneline --abbrev-commit'
+alias gmu='git fetch origin -v; git fetch upstream -v; git merge upstream/master'
+alias gp='git push'
+alias gpo='git push origin'
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias grt='cd $(git rev-parse --show-cdup)'
+alias gs='git status'
+alias gsfp='git stash show -p | git apply && git stash drop'
+alias gss='git status -s'
+alias gst='git status'
+alias gup='git pull --rebase'
+
+case $OSTYPE in
+  linux*)
+    alias gd='git diff | vim -R -'
+    ;;
+  darwin*)
+    alias gd='git diff | mate'
+    ;;
+  darwin*)
+    alias gd='git diff'
+    ;;
+esac
