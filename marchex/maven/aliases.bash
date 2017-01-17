592c7265e3aff84107d5fafbe80f770ee55df880
Anton DeVore <adevore@marchex.com>
2017-01-17 15:44:15 -0800

    Added aliases for using liquibase through maven

--- marchex/maven/aliases.bash
+++ marchex/maven/aliases.bash
@@ -5,3 +5,22 @@ alias mcit='mvn clean install -DskipTests=true'
 alias mcito='mvn clean install -DskipTests=true --offline'
 alias muci='mvn -U clean install'
 alias mvo='mvn verify --offline'
+
+# Liquibase commands
+alias update='mvn --offline liquibase:update -e'
+alias clearCheckSums='mvn --offline liquibase:clearCheckSums -e'
+alias updateTestingRollback='mvn --offline liquibase:updateTestingRollback -e'
+
+function rollback() {
+  if [ $# -ne 1 ]
+  then
+    echo "Error: No number specified."
+    return 1
+  fi
+  isNumber='^[0-9]+$'
+  if [[ $1 =~ $isNumber ]] ; then
+    mvn --offline liquibase:rollback -Dliquibase.rollbackCount=$1 -e
+  else
+    echo "'$1' is not a valid number"
+  fi
+}
