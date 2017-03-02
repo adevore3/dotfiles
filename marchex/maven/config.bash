316e01199d181684a7c7092c0a4868f066ae2938
Anton DeVore <adevore@marchex.com>
2017-03-01 16:22:51 -0800

    Added config set up for running maven

--- marchex/maven/config.bash
+++ marchex/maven/config.bash
@@ -1,8 +1,8 @@
-export M2_HOME="/opt/maven"
-export M2="$M2_HOME/bin"
+export M3_HOME="/opt/maven"
+export M3="$M3_HOME/bin"
 
 if [ -z "$PATH" ]; then
-  export PATH=$M2
+  export PATH=$M3
 else
-  export PATH="$PATH:$M2"
+  export PATH="$PATH:$M3"
 fi
