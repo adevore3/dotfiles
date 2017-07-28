793d7ffaf1a78007bdb0eba70b288ffd12151c23
Anton DeVore <adevore@marchex.com>
2017-07-28 11:48:54 -0700

    Added tmux aliases

--- tmux/aliases.bash
+++ tmux/aliases.bash
@@ -2,7 +2,9 @@ alias ta='tmux a -t'
 alias tda='tmux detach -a'
 alias tkp='tmux kill-pane -a'
 alias tks='tmux kill-session -t'
+alias tksw='tmux kill-session -t work'
 alias tmux="`which tmux` -2"
 alias tn='tmux new -s'
+alias tnw='tmux new -s work'
 
 alias cw='create_window_with_n_panes'
