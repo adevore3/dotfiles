10e9001baf8a8a3839748f31d1bfbbc3fcbae669
Anton DeVore <adevore@indeed.com>
2018-11-13 13:48:14 -0800

    Moved ssh_auth logic back to bashrc

--- indeed/config.bash
+++ indeed/config.bash
@@ -5,11 +5,3 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# Mostly works (sometimes expires, not sure why)
-if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-  eval `ssh-agent`
-  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-fi
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-ssh-add -l > /dev/null || ssh-add
-
