0667776be2680d9660a827227df11eb51b0ddc3f
Anton DeVore <adevore@indeed.com>
2018-11-13 11:39:02 -0800

    Created config for Indeed general stuff

---
+++ indeed/config.bash
@@ -0,0 +1,15 @@
+# Enable auto-completion for logvis-tail
+source <(logvis-tail --completion-bash)
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Mostly works (sometimes expires, not sure why)
+if [ ! -S ~/.ssh/ssh_auth_sock ]; then
+  eval `ssh-agent`
+  ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
+fi
+export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
+ssh-add -l > /dev/null || ssh-add
+
