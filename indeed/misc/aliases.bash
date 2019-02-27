334b53f44d52d879aea766a4c14c909a37b7b315
Anton DeVore <adevore@indeed.com>
2019-02-27 15:35:32 -0600

    Added alias olv

--- indeed/misc/aliases.bash
+++ indeed/misc/aliases.bash
@@ -12,6 +12,7 @@ alias ogmr='DISPLAY=:0 firefox -new-tab -url $GITLAB_BASE_URL/$(basename $(dirna
 alias ognmr='DISPLAY=:0 firefox -new-tab -url "$GITLAB_BASE_URL/$(basename $(dirname "$PWD"))/$(basename "$PWD")/merge_requests/new?utf8=%E2%9C%93&merge_request[source_branch]=$(git symbolic-ref --short HEAD)&merge_request[target_branch]=master&merge_request[title]=$(basename $(git symbolic-ref --short HEAD)): DESCRIPTION"'
 alias oog='f(){ DISPLAY=:0 firefox -new-tab -url "$OPENGROK_BASE_URL/source/search?project=%21all%21&q=%22$1%22&defs=&refs=&path=&hist=&type=$2"; unset -f f; }; f'
 alias osg='f(){ DISPLAY=:0 firefox -new-tab -url "https://sourcegraph.qa.indeed.net/search?q=$1"; unset -f f; }; f'
+alias olv=$'f(){ DISPLAY=:0 firefox -new-tab -url "https://squall.indeed.com/logviewer?type=$1&source=prod&checked=detailedUID&date=$(($(date --date \'-1 hour\' +%s%N)/1000000))&numRequested=10"; unset -f f; }; f'
 alias proc='f(){ DISPLAY=:0 firefox -new-tab -url $PROCTOR_BASE_URL/proctor/definition/$(echo $1 | tr "[:upper:]" "[:lower:]")?branch=${2:-trunk}; unset -f f; }; f'
 alias prod='DISPLAY=:0 firefox -new-tab -url https://$(basename "$PWD").sandbox.indeed.net'
 alias qa='DISPLAY=:0 firefox -new-tab -url https://$(basename "$PWD").sandbox.qa.indeed.net'
