f387d2a6856206e25ba981c90c808397fa533329
Anton DeVore <adevore@indeed.com>
2019-03-07 10:13:43 -0800

    Added aliases trwj & trwm

--- tmux/aliases.bash
+++ tmux/aliases.bash
@@ -9,5 +9,7 @@ alias tn='tmux new -s'
 alias tnw='tmux new -s work'
 alias trw='tmux rename-window'
 alias trwd='tmux rename-window $(basename "$PWD")'
+alias trwj='tmux rename-window $(if [ -e repo.cfg ]; then fgrep "jira=" repo.cfg | cut -d= -f2; else echo $(basename "$PWD"); fi)'
+alias trwm='tmux rename-window misc'
 
 alias cw='create_window_with_n_panes'
