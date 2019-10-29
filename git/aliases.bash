1dba320ca9f9bbcf663e0a8ee6111799f8ea48b1
Anton DeVore <adevore@indeed.com>
2019-10-29 10:33:16 -0700

    Updated alias gco and renamed alias gcorb -> gcor

--- git/aliases.bash
+++ git/aliases.bash
@@ -12,10 +12,10 @@ alias gbrlt='for k in `git branch -r | perl -pe "s/^..(.*?)( ->.*)?$/\1/"`; do e
 alias gbrv='git branch -rv'
 alias gbvv='git branch -vv'
 alias gcl='git clone'
-alias gco='f(){ local branch=$(git branch -r | grep -i $1 | tr -d "[:space:]"); git checkout $branch; unset -f f; }; f'
+alias gco='f(){ local branch=$(git branch | grep -i $1 | tr -d "[:space:]"); git checkout $branch; unset -f f; }; f'
 alias gcoad="git status | grep deleted | awk '{print \$2}' | xargs git checkout --"
 alias gcob='git checkout -b'
-alias gcorb='f(){ local branch=$(git branch -r | grep -i $1 | tr -d "[:space:]" | cut -c 8-); git checkout -b $branch origin/$branch; unset -f f; }; f'
+alias gcor='f(){ local branch=$(git branch -r | grep -i $1 | tr -d "[:space:]" | cut -c 8-); git checkout -b $branch origin/$branch; unset -f f; }; f'
 alias gcount='git shortlog -sn'
 alias gcp='git cherry-pick'
 alias gdv='git diff -w "$@" | vim -R -'
