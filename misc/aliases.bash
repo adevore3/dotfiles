b2beb6116a2f54dfc1afab2f0d9d0a87947940cd
Anton DeVore <adevore@indeed.com>
2019-11-04 09:24:51 -0800

    Updated alias pm and removed alias postman

--- misc/aliases.bash
+++ misc/aliases.bash
@@ -3,5 +3,4 @@ alias vn='vi $DOTFILES/misc/notes.txt'
 
 alias dbeaver='nohup sh -c ~/dbeaver/dbeaver > /dev/null 2>&1 &'
 alias first_sha='git log -n 1; git log -n 1 --format=%H | tr -d "\n" | tmux loadb -'
-alias pm='postman'
-alias postman='nohup sh -c "~/Postman/Postman \$*" > /dev/null 2>&1 &'
+alias pm='nohup sh -c "postman \$*" > /dev/null 2>&1 &'
