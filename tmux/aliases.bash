742208ca3f07254d6fbc4cc19b8c8d2c200ba29d
Anton DeVore <adevore@indeed.com>
2019-11-07 14:55:22 -0800

    Updated tmux conf and removed alias

--- tmux/aliases.bash
+++ tmux/aliases.bash
@@ -4,7 +4,6 @@ alias tda='tmux detach -a'
 alias tkp='tmux kill-pane -a'
 alias tks='tmux kill-session -t'
 alias tksw='tmux kill-session -t work'
-alias tmux="`which tmux` -2"
 alias tn='tmux new -s'
 alias tnw='tmux new -s work'
 alias trw='tmux rename-window'
