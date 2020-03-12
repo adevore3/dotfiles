ee83646449ae0e838678ecf99a0f9519084ebefd
Anton DeVore <adevore@indeed.com>
2020-03-12 10:55:01 -0700

    Fixed docker aliases

--- docker/aliases.bash
+++ docker/aliases.bash
@@ -5,8 +5,8 @@ alias dri=docker_remove_all_images
 alias dsc=docker_stop_all_containers
 
 # long aliases
-alias docker_kill_all_containers="docker kill $(docker ps -q)" # kill all containers
-alias docker_remove_all_containers="docker rm $(docker ps -a -q)" # remove all containers
-alias docker_remove_all_images="docker rmi $(docker images -q)" # remove all docker images
-alias docker_stop_all_containers="docker stop $(docker ps -q)" # stop all containers
+alias docker_kill_all_containers='docker kill $(docker ps -q)' # kill all containers
+alias docker_remove_all_containers='docker rm $(docker ps -a -q)' # remove all containers
+alias docker_remove_all_images='docker rmi $(docker images -q)' # remove all docker images
+alias docker_stop_all_containers='docker stop $(docker ps -q)' # stop all containers
 
