2ebbec3d194a8d3ce319dacbff9458cdce41ce92
Anton DeVore <adevore@indeed.com>
2020-03-28 17:12:19 -0700

    Added aliases vOggl & voggl

--- vim/aliases.bash
+++ vim/aliases.bash
@@ -1,2 +1,4 @@
+alias vOggl='f(){ vi -O $(git grep -l "$*"); unset -f f; }; f'
 alias vggl='f(){ vi $(git grep -l "$*"); unset -f f; }; f'
+alias voggl='f(){ vi -o $(git grep -l "$*"); unset -f f; }; f'
 
