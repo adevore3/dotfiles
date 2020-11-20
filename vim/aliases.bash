d6a59da4e2ecce7fec17979ba86ac27804555afd
Anton DeVore <adevore@indeed.com>
2020-11-20 15:30:19 -0800

    Add alias vf

--- vim/aliases.bash
+++ vim/aliases.bash
@@ -1,4 +1,5 @@
 alias vOggl='f(){ vi -O $(git grep -l "$*"); unset -f f; }; f'
+alias vf='f(){ vi $(find . -name "$*"); unset -f f; }; f'
 alias vggl='f(){ vi $(git grep -l "$*"); unset -f f; }; f'
 alias voggl='f(){ vi -o $(git grep -l "$*"); unset -f f; }; f'
 
