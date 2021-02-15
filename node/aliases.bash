67b46c13f8f503085420b8484dc063f694849d78
Anton DeVore <adevore@indeed.com>
2021-02-15 12:00:21 -0800

    Add alias for common node & npm commands

--- node/aliases.bash
+++ node/aliases.bash
@@ -1,2 +1,8 @@
+alias nag='ag "npm"'
+alias nls='if command -v list-scripts &> /dev/null; then list-scripts; else echo "list-scripts has not been installed, run: npm install -g list-scripts"; fi'
 alias nu='cdnvm .'
+
+# Built on pre-existing aliases
+alias nr?='nrh'
+alias nrh='nls && echo -e "\\nag \"npm\"" && nag'
 
