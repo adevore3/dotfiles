4897458f8e582d3b79ce2033389ce54812f458e6
Anton DeVore <adevore@indeed.com>
2021-09-22 15:21:51 -0700

    Update docker aliases

--- docker/aliases.bash
+++ docker/aliases.bash
@@ -1,12 +1,17 @@
 # short aliases
-alias dkc=docker_kill_all_containers
-alias drc=docker_remove_all_containers
-alias dri=docker_remove_all_images
-alias dsc=docker_stop_all_containers
+alias dkac=docker_kill_all_containers
+alias drac=docker_remove_all_containers
+alias drai=docker_remove_all_images
+alias druc=docker_remove_unused_containers
+alias drui=docker_remove_unused_images
+alias dsac=docker_stop_all_containers
 
 # long aliases
-alias docker_kill_all_containers='docker kill $(docker ps -q)' # kill all containers
-alias docker_remove_all_containers='docker rm $(docker ps -a -q)' # remove all containers
-alias docker_remove_all_images='docker rmi $(docker images -q)' # remove all docker images
-alias docker_stop_all_containers='docker stop $(docker ps -q)' # stop all containers
+alias docker_kill_all_containers='docker kill $(docker ps -q)'
+alias docker_nuke='docker system prune'
+alias docker_remove_all_containers='docker rm $(docker ps -a -q)'
+alias docker_remove_all_images='docker rmi $(docker images -q)'
+alias docker_remove_unused_containers='docker rm $(docker ps -qa --no-trunc --filter "status=exited")'
+alias docker_remove_unused_images='docker rmi $(docker images --filter "dangling=true" -q --no-trunc)'
+alias docker_stop_all_containers='docker stop $(docker ps -q)'
 
