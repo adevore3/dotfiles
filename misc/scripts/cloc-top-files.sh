fe16d30f3ba1b35095f49608baaedc4980d6a1b8
Anton DeVore <adevore@indeed.com>
2022-01-13 12:47:49 -0800

    Add script to cloc specific files

---
+++ misc/scripts/cloc-top-files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script
+
+if ! [ -x "$(command -v cloc)" ]; then
+  echo "'cloc' command needed to calculate lines"
+fi
+
+desired_num_files=5
+
+undesired_file_types="json|png|svg|snap|po"
+
+desired_files=$(git ls-files | rev | cut -d'.' -f1 | rev | sort | uniq -c | sort -rg | egrep -v "($undesired_file_types)" | head -n $desired_num_files | awk '{print $2}' | sed -e 's/$/\$/' | sed -e 's/^/\./' | sed ':a; N; $!ba; s/\n/|/g')
+
+echo "desired files: '$desired_files'"
+
+desired_git_files=$(git ls-files | egrep "($desired_files)")
+
+cloc $desired_git_files
+
