47f7043a7dd1408cf6277a6d5993f6a1878e673a
Anton DeVore <adevore@indeed.com>
2022-01-13 11:39:52 -0800

    Add alias cloch

--- misc/aliases.bash
+++ misc/aliases.bash
@@ -1,6 +1,7 @@
 alias cn='cat $DOTFILES/misc/notes.txt'
 alias vn='vi $DOTFILES/misc/notes.txt'
 
+alias cloch='cloc $(git ls-tree --name-only -r HEAD)'
 alias dbeaver='nohup sh -c ~/dbeaver/dbeaver > /dev/null 2>&1 &'
 alias first_sha='git log -n 1; git log -n 1 --format=%H | tr -d "\n" | tmux loadb -'
 alias pm='nohup sh -c "postman \$*" > /dev/null 2>&1 &'
