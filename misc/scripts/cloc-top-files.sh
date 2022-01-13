33930597e4438ea96f0d2e550fe63cd2fa11ab35
Anton DeVore <adevore@indeed.com>
2022-01-13 13:49:22 -0800

    Update cloc script

--- misc/scripts/cloc-top-files.sh
+++ misc/scripts/cloc-top-files.sh
@@ -10,7 +10,14 @@ desired_num_files=5
 
 undesired_file_types="json|png|svg|snap|po"
 
-desired_files=$(git ls-files | rev | cut -d'.' -f1 | rev | sort | uniq -c | sort -rg | egrep -v "($undesired_file_types)" | head -n $desired_num_files | awk '{print $2}' | sed -e 's/$/\$/' | sed -e 's/^/\./' | sed ':a; N; $!ba; s/\n/|/g')
+# 1. Find unique file types
+# 2. Count & sort file types
+# 3. Filter & limit file types
+# 4. Transform file types for filter
+desired_files=$(git ls-files | rev | cut -d'.' -f1 | rev \
+  | sort | uniq -c | sort -rg \
+  | egrep -v "($undesired_file_types)" | head -n $desired_num_files \
+  | awk '{print $2}' | sed -e 's/$/\$/' | sed -e 's/^/\./' | sed ':a; N; $!ba; s/\n/|/g')
 
 echo "desired files: '$desired_files'"
 
