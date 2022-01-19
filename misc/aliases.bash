8de09dfa0f7889108eccc8f6e917a10f3eb9739f
Anton DeVore <adevore@indeed.com>
2022-01-19 15:10:54 -0800

    Add alias ctf

--- misc/aliases.bash
+++ misc/aliases.bash
@@ -1,6 +1,7 @@
 alias cn='cat $DOTFILES/misc/notes.txt'
 alias vn='vi $DOTFILES/misc/notes.txt'
 
+alias ctf='~/dotfiles/misc/scripts/cloc-top-files.sh'
 alias cloch='cloc $(git ls-files)'
 alias dbeaver='nohup sh -c ~/dbeaver/dbeaver > /dev/null 2>&1 &'
 alias first_sha='git log -n 1; git log -n 1 --format=%H | tr -d "\n" | tmux loadb -'
