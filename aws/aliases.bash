d8935c6e5d72f7dc4a61f7fd28840727fd2f0cac
Anton DeVore <adevore@indeed.com>
2022-08-30 14:12:02 -0700

    Add & reorganize aws aliases

---
+++ aws/aliases.bash
@@ -0,0 +1,8 @@
+alias acl='aws configure list'
+alias aclp='aws configure list-profiles'
+alias asgci='aws sts get-caller-identity'
+alias caco='cat ~/.aws/config'
+alias cacr='cat ~/.aws/credentials'
+alias vaco='vi ~/.aws/config'
+alias vacr='vi ~/.aws/credentials'
+
