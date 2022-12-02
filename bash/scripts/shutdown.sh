142132edffba5f6ffd6f3e1680a906d04382a596
Anton DeVore <adevore@indeed.com>
2022-12-02 15:46:12 -0800

    Add startup/shutdown scripts & aliases

---
+++ bash/scripts/shutdown.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+kp brave-browser
+kp idea
+kp google
+kp firefox
+kp mysql
+kp postman
+
+restart_now
+
---
+++ bash/scripts/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+brave-browser > /dev/null 2>&1 &
+intellij-idea-ultimate > /dev/null 2>&1 &
+google-chrome > /dev/null 2>&1 &
+firefox > /dev/null 2>&1 &
+
