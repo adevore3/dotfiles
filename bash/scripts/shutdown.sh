d77b304e2bcbf62cd42e7e90b79209e42d2ad413
Anton DeVore <adevore@indeed.com>
2022-12-05 11:51:26 -0800

    Update shutdown script

--- bash/scripts/shutdown.sh
+++ bash/scripts/shutdown.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-kp brave-browser
-kp idea
-kp google
-kp firefox
-kp mysql
-kp postman
+kill_process() {
+  kill $(ps aux | grep -v grep | grep $1 | awk '{print $2}')
+}
 
-restart_now
+kill_process brave-browser
+kill_process idea
+kill_process google
+kill_process firefox
+kill_process mysql
+kill_process postman
+
+sudo shutdown -r now
 
