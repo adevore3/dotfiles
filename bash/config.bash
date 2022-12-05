7c87040b7a7a79aa1184b87aee717a45babc80be
Anton DeVore <adevore@indeed.com>
2022-12-05 12:09:45 -0800

    Update HISTIGNORE

--- bash/config.bash
+++ bash/config.bash
@@ -23,7 +23,7 @@ shopt -s histappend
 
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
-export HISTIGNORE='\.\.:\.\.\.:ag*:exit:gst:gup::history*:htop:ij:ll:ls:tnw:tnwi:umr:[ ]*'
+export HISTIGNORE='\.\.:\.\.\.:ag*:exit:gst:gup:htop:ij:ll:ls:tnw:tnwi:umr:[ ]*'
 export HISTTIMEFORMAT="%c "
 export HISTCONTROL=erasedups
 
