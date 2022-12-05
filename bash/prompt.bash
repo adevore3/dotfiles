d8db7db4f1bd80a4b69363541ad702eda439054c
Anton DeVore <adevore@indeed.com>
2022-12-05 11:52:00 -0800

    Save & refresh bash history after each command

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -31,4 +31,5 @@ function prompt() {
     PS1="\n${n_commands} ${user_host} ${prompt_symbol} $node ${open}${current_path}${git_branch}${close}${return_status}\n${prompt_char}"
 }
 
-PROMPT_COMMAND=prompt
+PROMPT_COMMAND="prompt;history -a;history -r"
+
