6b9a30baee2800edaf07f78b3833b421f7d142e2
Anton DeVore <adevore@indeed.com>
2023-06-27 11:30:53 -0700

    Improve cacrg alias

--- aws/aliases.bash
+++ aws/aliases.bash
@@ -3,7 +3,7 @@ alias aclp='aws configure list-profiles'
 alias asgci='aws sts get-caller-identity'
 alias caco='cat ~/.aws/config'
 alias cacr='cat ~/.aws/credentials'
-alias cacrg='f(){ cat ~/.aws/credentials | grep -A 4 $1; unset -f f; }; f'
+alias cacrg='f(){ cat ~/.aws/credentials | grep -A 4 $1 | tee >(tmux loadb -) | tee >(xclip -selection c); unset -f f; }; f'
 alias vaco='vi ~/.aws/config'
 alias vacr='vi ~/.aws/credentials'
 
