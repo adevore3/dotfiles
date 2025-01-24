617f81c3afa1e5f30c9f6ab515591eb1c67f6058
Anton DeVore <adevore@indeed.com>
2025-01-24 14:40:42 -0800

    Add script to trim ssd; add support for loading crons

---
+++ bash/cron/aliases.bash
@@ -0,0 +1,11 @@
+# history commands
+alias find_dotfile_crons='find ${DOTFILES} -type f -name cron'
+alias update_crons='f(){ \
+  local -r crontab_file="${DOTFILES}/bash/cron/crontab-final"; \
+  echo > $crontab_file; \
+  find ${DOTFILES} -type f -name cron | grep -v cloudvm | xargs -I {} cat {} >> $crontab_file; \
+  crontab $crontab_file; \
+  crontab -l; \
+  unset -f f; \
+}; f'
+
---
+++ bash/scripts/trim-ssd.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+fstrim -av >> /var/log/trim-ssd.log 2>&1
+
