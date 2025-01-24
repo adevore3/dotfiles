afaa7f563410c9d0c660b956992fad1460d451a4
Anton DeVore <adevore@indeed.com>
2025-01-24 14:46:59 -0800

    Clean up update crons alias

--- bash/cron/aliases.bash
+++ bash/cron/aliases.bash
@@ -3,7 +3,7 @@ alias find_dotfile_crons='find ${DOTFILES} -type f -name cron'
 alias update_crons='f(){ \
   local -r crontab_file="${DOTFILES}/bash/cron/crontab-final"; \
   echo > $crontab_file; \
-  find ${DOTFILES} -type f -name cron | grep -v cloudvm | xargs -I {} cat {} >> $crontab_file; \
+  find ${DOTFILES} -type f -name cron | xargs -I {} cat {} >> $crontab_file; \
   crontab $crontab_file; \
   crontab -l; \
   unset -f f; \
